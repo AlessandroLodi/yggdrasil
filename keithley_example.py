562956d3aa1780df5aab2a02b838563b733568f2
alessandro-lodi <lodialessandro0@gmail.com>
2024-08-28 19:20:03 +0200

    porting looks OK

--- keithley_example.py
+++ keithley_example.py
@@ -1,30 +1,30 @@
 import pyvisa
-
+from qcodes.instrument_drivers.Keithley.Keithley_2400 import Keithley2400
 rm = pyvisa.ResourceManager()
+
 print(rm.list_resources())
 
-# Create an instance of the Keithley 2400 driver
-# Replace 'GPIB0::25::INSTR' with the actual VISA address of your instrument
-# k2400 = Keithley2400('k2400', 'GPIB0::25::INSTR')
+# Initialize the Keithley2400
+k2400 = Keithley2400('k2400', 'GPIB0::1::INSTR')
 
-# # Set the source mode to voltage
-# k2400.source_mode('voltage')
+# Set the voltage to 1V
+k2400.volt(1)
 
-# # Set the voltage to 1V
-# k2400.source_voltage(1)
+# Set the current compliance to 10mA
+k2400.compliancei(0.01)
 
-# # Set the current compliance to 10mA
-# k2400.current_range(0.01)
+# Set the mode to voltage source
+k2400.mode('VOLT')
 
-# # Enable the output
-# k2400.output_enabled(True)
+# Enable the output
+k2400.output(True)
 
-# # Measure the current
-# current = k2400.current()
-# print(f"Measured current: {current} A")
+# Measure the current
+current = k2400.curr()
+print(f"Measured current: {current} A")
 
-# # Disable the output
-# k2400.output_enabled(False)
+# Disable the output
+k2400.output(False)
 
-# # Close the connection when done
-# k2400.close()
+# Close the instrument connection
+k2400.close()
